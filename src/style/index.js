4eaff900d0409eb8f95920bcbaa910979eb89f80
bernardstanislas <bernardstanislas@gmail.com>
2016-02-17 16:08:18 +0100

    Add font awesome

--- src/style/index.js
+++ src/style/index.js
@@ -3,3 +3,4 @@ import './demo.scss';
 import 'focus-components/style';
 import 'highlight.js/styles/default.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.scss';
+import 'font-awesome/css/font-awesome.min.css';
