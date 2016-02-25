869a0f8494e8dd390bdc59972ce9d4aeb516d0ea
Pierre Besson <pierre.besson7@gmail.com>
2016-02-25 09:51:22 +0100

    [test]

--- test/simple.js
+++ test/simple.js
@@ -65,44 +65,40 @@ describe('testing internet explorer', () => {
 
 const testGenerator = browsers => {
   browsers.forEach(browserName => {
+    describe(`testing javascript in ${browserName}`, () => {
+            beforeEach(() => {
+              if (process.env.SAUCE_USERNAME != undefined) {
+                 this.browser = new webdriver.Builder()
+                 .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+                 .withCapabilities({
+                   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+                   build: process.env.TRAVIS_BUILD_NUMBER,
+                   username: process.env.SAUCE_USERNAME,
+                   accessKey: process.env.SAUCE_ACCESS_KEY,
+                   browserName: browserName
+                 }).build();
+               } else {
+                 this.browser = new webdriver.Builder()
+                 .withCapabilities({
+                   browserName: browserName
+                 }).build();
+               }
 
-describe("testing javascript in " + browserName, function() {
-        beforeEach(function() {
-       if (process.env.SAUCE_USERNAME != undefined) {
-         this.browser = new webdriver.Builder()
-         .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
-         .withCapabilities({
-           'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-           build: process.env.TRAVIS_BUILD_NUMBER,
-           username: process.env.SAUCE_USERNAME,
-           accessKey: process.env.SAUCE_ACCESS_KEY,
-           browserName: browserName
-         }).build();
-       } else {
-         this.browser = new webdriver.Builder()
-         .withCapabilities({
-           browserName: browserName
-         }).build();
-       }
+              return this.browser.get("http://getfocus.io");
+             });
 
-       return this.browser.get("http://getfocus.io");
-     });
+           afterEach(() => this.browser.quit());
 
-     afterEach(function() {
-       return this.browser.quit();
-     });
+         it('should handle clicking on a headline', done => {
+           const headline = this.browser.findElement(webdriver.By.css('h3'));
+           headline.click();
+           headline.getText().then(txt => {
+             assert.equal(txt, "Les librairies FOCUS");
+             done();
+           });
+         });
+    });
+  });
+};
 
-     it("should handle clicking on a headline", function(done) {
-       var headline = this.browser.findElement(webdriver.By.css('h3'));
-       headline.click();
-       headline.getText().then(function(txt) {
-         assert.equal(txt, "Les librairies FOCUS");
-         done();
-       });
-     });
-});
-
-
-  }
-}
 testGenerator(['chrome', 'firefox', 'internet explorer', 'iPhone', 'iPad']);
