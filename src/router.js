8d2671aec367eb277df9484dd777a477fc16af35
GuenoleK <kikabouguenole@gmail.com>
2016-05-03 11:55:51 +0200

    Updating router by removing useless calls, imports, and routes

--- src/router.js
+++ src/router.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Catalog from './catalog';
 import Layout from './layout';
-import NewLayout from './new-layout';
-import Test from './test';
 import Sandbox from './live-component';
 import componentsStore from './store/components';
 import getComponentFromName  from './service/get-component-from-name';
@@ -18,29 +16,11 @@ const links = catalog.map((component, idx) => ({url: `#query/${component.name}`,
 const ShowCaseRouter = router.extend({
     routes: {
         '': 'showcase',
-        'test': 'GoToText',
-        'new-layout': 'GoToLayout',
         'component/:name': 'component',
         'component/:name/detail': 'componentDetail',
         'query': 'showcase',
         'query/:query': 'query',
         '*notFound': 'showcase'
-    },
-    GoToText() {
-        console.log('test page');
-        // render the showcase into the document
-        return ReactDOM.render(
-            <Test links={links}><Catalog store={componentsStore} query=''/></Test>,
-            document.querySelector(`.${__ANCHOR_CLASS__}`)
-        );
-    },
-    GoToLayout() {
-        console.log('NewLayout');
-        // render the showcase into the document
-        return ReactDOM.render(
-            <NewLayout title='Component catalog' links={links}><Test/></NewLayout>,
-            document.querySelector(`.${__ANCHOR_CLASS__}`)
-        );
     },
     showcase(){
         console.log('showcase');
