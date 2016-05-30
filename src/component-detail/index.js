5d28fd510d5fbff3e5435b2afafbef7f8825d971
GuenoleK <kikabouguenole@gmail.com>
2016-05-30 20:10:57 +0200

    Updating the sliding right menu

--- src/component-detail/index.js
+++ src/component-detail/index.js
@@ -31,6 +31,27 @@ class ComponentDetail extends Component{
         }
     }
 
+    toggleOpen() {
+        if(document.getElementById("mySidenav").style.width !== "250px" && document.getElementById("main").style.marginRight !== "250px") {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("main").style.marginRight = "250px";
+        }
+        else {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginRight = "0";
+        }
+    }
+
+    openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginRight = "250px";
+    }
+
+    closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginRight = "0";
+    }
+
     /** @inheriteDoc */
     render() {
         const {name, capture, description, example, photo, keywords, version, code} = this.props;
@@ -47,7 +68,7 @@ class ComponentDetail extends Component{
                         <div data-focus='title'>
                             <h2>{`${capitalize(name)} - v${version}`}</h2>
                         </div>
-                        <button className='mdl-button mdl-js-button mdl-button--fab mdl-button--accent mdl-js-ripple-effect' onClick={() => { this.isChecked() } } data-focus='demo-button'>
+                        <button className='mdl-button mdl-js-button mdl-button--fab mdl-button--accent mdl-js-ripple-effect' onClick={() => { this.toggleOpen() } } data-focus='demo-button'>
                             <i className="material-icons">code</i>
                         </button>
                     </div>
@@ -75,26 +96,16 @@ class ComponentDetail extends Component{
                     </div>
                 </div>
 
-                <div className='myCustomBody'>
-                    <div className="navigation">
-                        <textarea></textarea>
-                    </div>
+                <div id="mySidenav" className="sidenav">
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Clients</a>
+                    <a href="#">Contact</a>
+                </div>
 
-                    <input type="checkbox" id="nav-trigger" className="nav-trigger" />
-
-                    <div className="site-wrap">
-                        <h1>Pure CSS Off-Screen Menu</h1>
-                        <h3>Finally, an off-screen menu that doesn't require a bunch of Javascript to work. </h3>
-
-                        <p>This concept relies on the <code>:checked</code> pseudo-selector as well as the general sibling <code>~</code> selector, so it has decent browser support.</p>
-                        <p><strong>Browsers supported:</strong> IE9+, Firefox 3.5+, Chrome any, Safari 3.2+, Opera 9.5+</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vero nisi eos sed qui natus, ut eius reprehenderit error nesciunt veniam aliquam nulla itaque labore obcaecati molestiae eveniet, perferendis provident amet perspiciatis expedita accusantium! Eveniet, quos voluptas et, labore natus, saepe unde est nulla sit eaque tempore debitis accusantium. Recusandae.</p>
-                        <p>Dolorem aliquam a libero reiciendis obcaecati doloribus ipsa eos laudantium, dicta in! Odit iure ut ratione, dolorum cupiditate perferendis voluptatum sapiente, dignissimos sunt necessitatibus, reprehenderit consequatur dolorem. Aliquam veniam quaerat, pariatur deserunt reiciendis vero vitae, repellat omnis sequi dolor nesciunt. Nihil similique alias impedit, obcaecati eligendi delectus voluptatum! Ipsum, vel.</p>
-                        <p>Sint, perspiciatis nemo aut, rerum excepturi deleniti modi quos nihil corporis eum, maiores soluta labore, consectetur eligendi nesciunt. Placeat, incidunt! Illum placeat eligendi, veritatis consectetur eum! Dolor obcaecati minima ab placeat voluptatem neque modi doloribus, magnam qui voluptate eaque in. Nulla expedita hic porro architecto facere officiis vitae numquam, dolor!</p>
-                        <p>Perferendis quis ea incidunt ducimus nisi voluptate natus. Repellat asperiores quod rerum rem quos blanditiis enim modi, veniam voluptas a facilis! Velit cum omnis, voluptatum eum inventore! Corrupti, suscipit, neque distinctio expedita est laboriosam cum aliquid minus tempora quaerat officia possimus unde vel deleniti eaque fugit accusamus iusto dolorum natus.</p>
-
-                        <p>Demo by Austin Wulf. <a href="http://www.sitepoint.com/pure-css-off-screen-navigation-menu">See article</a>.</p>
-                    </div>
+                <div id="main">
+                    <h2>Sidenav Push Example</h2>
+                    <p>Click on the element below to open the side navigation menu, and push this content to the right.</p>
                 </div>
             </div>
         );
